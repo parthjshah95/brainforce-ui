8fe4a42c96e7567619df4caf98dd00581a8b0cb6
Parth Shah <parthjshah95@gmail.com>
2020-12-21 02:55:53 -0500

    adding simple keyboard

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container" :style="show_cursor?'':no_cursor_style">
+    <!-- <Block/>
+    <Block/>
+    <Block/>
+    <Block/>
+    <Block/>
+    <Block/> -->
+    <input
+      :value="input"
+      class="input"
+      @input="onInputChange"
+      placeholder="Tap on the virtual keyboard to start"
+    >
+    <SimpleKeyboard @onChange="onChange" @onKeyPress="onKeyPress" :input="input"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import SimpleKeyboard from "./components/SimpleKeyboard";
+
+let show_cursor = true
+// let show_cursor = false
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    SimpleKeyboard
+  },
+  data(){
+    return {
+      no_cursor_style: "cursor: none;",
+      show_cursor: show_cursor,
+      input: ""
+    }
+  },
+  methods: {
+    onChange(input) {
+      this.input = input;
+    },
+    onKeyPress(button) {
+      console.log("button", button);
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container{
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  align-content: center;
+  background-color: #eee;
+}
+body{
+  height:100%;
+}
+html{
+  height:100%;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+$base-width: 200px;
+$base-height: 120px;
+$base-margin: 20px;
+$expansion: 40px;
+
+$border-radius: 10px;
+$box-shadow: 0 5px 20px rgba(0,0,0,0.19), 0 3px 6px rgba(0,0,0,0.23);
+$box-shadow-highlight: 0 5px 30px rgba(0,0,0,0.19), 0 3px 15px rgba(0,0,0,0.23);
+
+.card{
+    margin: $base-margin;
+    border-radius: $border-radius;
+    box-shadow: $box-shadow;
+    width: $base-width;
+    height: $base-height;
+    transition: 0.3s;
+    background-color: white;
+    &:hover{
+      transition: 0.3s;
+      width: $base-width + $expansion;
+      height: $base-height + $expansion;
+      margin: $base-margin - ($expansion/2);
+    }
+}
+
+</style>
