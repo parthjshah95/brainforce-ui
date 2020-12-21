a70f393387071485f64435a613dfbcc322cde1ce
Parth Shah <parthjshah95@gmail.com>
2020-12-21 03:21:33 -0500

    'released_on_2020-12-21'

---
+++ docs/precache-manifest.8a08c007974c5ed4cf8f73d72370ad2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056051e647e4ac5e7173",
+    "url": "/css/app.c6349e8b.css"
+  },
+  {
+    "revision": "fefec5d144e668ff014aa82872cdd840",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056051e647e4ac5e7173",
+    "url": "/js/app.9f7ed855.js"
+  },
+  {
+    "revision": "3457ffba2ec6b1ac1e8e",
+    "url": "/js/chunk-vendors.df5fe186.js"
+  },
+  {
+    "revision": "71f8ffa74b6411dd85269a26d16c4138",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
